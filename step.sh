bef40101418aef8296bcdf2ec47496479f945a1b
Juba <abdelrahman.fawzy@cassbana.com>
2023-08-20 01:40:55 +0300

    create step

---
+++ step.sh
@@ -0,0 +1,69 @@
+#!/bin/bash
+set -ex
+
+state=$devops_pr_state
+description=$devops_description
+
+if [[ "$state" == "auto" ]]
+then
+    state="failed"
+
+    if [[ $BITRISE_BUILD_STATUS -eq 0 ]]
+    then
+        state="succeeded"
+    fi
+fi
+
+if [[ -z "$description" ]]
+then
+    description="${BITRISE_APP_TITLE} build #${BITRISE_BUILD_NUMBER} ${state}"
+fi
+
+
+ITERATIONS_URL="https://dev.azure.com/${organization_name}/${project_name}/_apis/git/repositories/${repository_name}/pullRequests/${BITRISE_PULL_REQUEST}/iterations?api-version=7.0"
+
+
+HTTP_STATUS=$(
+    curl -u :${azure_pat} -s -o response.json -w "%{http_code}" $ITERATIONS_URL \
+    -H "Content-Type: application/json"
+)
+
+
+if [ $HTTP_STATUS != "200" ]; 
+then
+    echo "Error [HTTP status: $HTTP_STATUS]"
+    exit 1
+fi
+
+echo "Server returned:  "
+
+cat response.txt
+
+ITERATIONS_COUNT=$(jq .count response.json)   
+
+
+UPDATE_PR_STATUS_URL="https://dev.azure.com/${organization_name}/${project_name}/_apis/git/repositories/${repository_name}/pullRequests/${BITRISE_PULL_REQUEST}/iterations/${ITERATIONS_COUNT}/statuses?api-version=7.0"
+
+
+HTTP_STATUS2=$(
+    curl -u :${azure_pat} -s -w "%{http_code}" $UPDATE_PR_STATUS_URL \
+    -H "Content-Type: application/json" \
+    -d @- <<EOF
+    {
+        "state": "${state}",
+        "description": "${description}",
+        "targetUrl": "${BITRISE_BUILD_URL}",
+        "context":  {
+            "genre": "${devops_context_genre}",
+            "name": "${devops_context_name}"
+        }
+    }
+EOF
+)
+
+
+if [ $HTTP_STATUS2 != "200" ]; 
+then
+    echo "Error [HTTP status: $HTTP_STATUS2]"
+    exit 1
+fi
